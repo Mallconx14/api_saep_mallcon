13a3410b51de36ff1987bdca6f9aa27f713c0081
Mallconx14 <mallconx14@gmail.com>
2026-05-26 15:40:04 -0300

    Rotas finalizadas

---
+++ routes/estoque.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+
+router.get('/produtos', (req, res) => {
+    db.query('SELECT * FROM produtos', (err, results) => {
+        if (err) {
+            res.status(500).json({ error: 'Erro ao buscar usuários' });
+        } else {
+            res.json(results);
+        }
+    });
+}); 
+
+router.get('/categoria', (req, res) => {
+    db.query('SELECT categoria, SUM(quantidade * valor_unidade) AS total_categoria FROM produtos GROUP BY categoria', (err, results) => {
+        if (err) {
+            res.status(500).json({ error: 'Erro ao buscar usuários' });
+        } else {
+            res.json(results);
+        }
+    });
+}); 
+
+router.post('/cadastrar', (req, res) => {
+    const { nome, quantidade, valor_unidade, categoria } = req.body;
+    db.query('INSERT INTO produtos (nome, quantidade, valor_unidade, categoria) VALUES (?, ?, ?, ?)', [nome, quantidade, valor_unidade, categoria], (err, results) => {
+        if (err) {
+            res.status(500).json({ error: 'Erro ao inserir informações', err});
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+router.post('/registro', (req, res) => {
+    const { dt, tipo, quantidade, id_produtos } = req.body;
+    db.query('INSERT INTO movimentacoes (dt, tipo, quantidade, id_produtos) VALUES (?, ?, ?, ?)', [dt, tipo, quantidade, id_produtos], (err, results) => {
+        if (err) {
+            res.status(500).json({ error: 'Erro ao inserir informações', err});
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+
+module.exports = router;
